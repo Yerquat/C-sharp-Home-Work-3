f387e568895fb88df2aba89cd4b44d6901543fa2
Yerquat <yernur75@gmail.com>
2022-12-09 00:03:19 +0600

    ex3.2

---
+++ exercise_3.2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+double distance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2) );
+return result;
+}
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
