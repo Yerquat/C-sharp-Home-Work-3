4d0e60234af293ccac366f2ed45b3403d29267a6
Yerquat <yernur75@gmail.com>
2022-12-09 00:02:28 +0600

    ex3.1

---
+++ exercise_3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+;
+    //string number()
+    string DetPolyndrom(int n) 
+     {
+        string result = "не полиндром";
+        if((n / 10000 == n % 10) && ((n / 1000) % 10 == (n / 10) % 10)) 
+            result = "полиндром";
+        return result;
+     }
+     
+    
+    int EnterData(string text)
+    {
+        Console.WriteLine(text);
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    } 
+     
+       
+        int n = EnterData("Введите число ");
+        string number = DetPolyndrom(n);
+        Console.WriteLine("Введенное число " + number);
